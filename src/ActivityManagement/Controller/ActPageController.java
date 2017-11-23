ce3231be9a0648dec634d54d5e850360c77c2a62
eakarin01 <ong_eak@hotmail.com>
2017-11-23 18:20:03 +0700

    coding set anchor pane

--- src/ActivityManagement/Controller/ActPageController.java
+++ src/ActivityManagement/Controller/ActPageController.java
@@ -3,6 +3,7 @@ package ActivityManagement.Controller;
 import ActivityManagement.MainProgram;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
@@ -34,6 +35,7 @@ public class ActPageController implements Reloadable{
         // remain anchor with children when add new node
         mainactpane.getChildren().clear();
         mainactpane.getChildren().add(MainProgram.memberactpane);
+        fitNodetoParent(MainProgram.memberactpane);
         MainProgram.stageMemberActPane.reloadPage();
     }
 
@@ -41,6 +43,15 @@ public class ActPageController implements Reloadable{
     void calltoShowDepartmentPane(ActionEvent event) {
         mainactpane.getChildren().clear();
         mainactpane.getChildren().add(MainProgram.DeptPane);
+        fitNodetoParent(MainProgram.DeptPane);
         MainProgram.stageDeptPane.reloadPage();
+    }
+
+    void fitNodetoParent(Node nd)
+    {
+        AnchorPane.setTopAnchor(nd,0.0);
+        AnchorPane.setLeftAnchor(nd,0.0);
+        AnchorPane.setRightAnchor(nd,0.0);
+        AnchorPane.setBottomAnchor(nd,0.0);
     }
 }
